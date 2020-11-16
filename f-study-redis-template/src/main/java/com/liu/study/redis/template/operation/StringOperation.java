4c72c2a8b99efe8a3c2c72de12248812e452ff91
liuweian <1qaz2wsx>
2020-11-16 13:13:21 +0800

    redis

--- f-study-redis-template/src/main/java/com/liu/study/redis/template/operation/StringOperation.java
+++ f-study-redis-template/src/main/java/com/liu/study/redis/template/operation/StringOperation.java
@@ -2,6 +2,7 @@ package com.liu.study.redis.template.operation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
@@ -24,7 +25,10 @@ public class StringOperation {
     public void testExpire() {
         redisTemplate.expire("first:redis:lie", 23, TimeUnit.SECONDS);
 
-        // redisTemplate.opsForValue();
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+
+
+
         // redisTemplate.boundValueOps();
     }
 
