60b558f18f138b41dd7ae392f5f60a7f95dba7d4
aslangas <1474673858@qq.com>
2020-11-17 12:50:11 +0800

    2020-11-17

---
+++ g-study-redis-lock/src/main/java/com/liu/study/redis/lock/StudyRedisLockApplication.java
@@ -0,0 +1,10 @@
+package com.liu.study.redis.lock;
+
+/**
+ * 
+ * @author Liuweian
+ * @createTime 2020/11/16 20:08
+ * @version 1.0.0
+ */
+public class StudyRedisLockApplication {
+}
---
+++ g-study-redis-lock/src/main/java/com/liu/study/redis/lock/first/FirstRedisLock.java
@@ -0,0 +1,14 @@
+package com.liu.study.redis.lock.first;
+
+/**
+ * <note>使用setnx构建简单的redis的分布式锁</note>
+ *
+ * 存在的问题：
+ *      1、如果业务时间过长，超过设置的过期的时间，
+ * 
+ * @author Liuweian
+ * @createTime 2020/11/16 20:08
+ * @version 1.0.0
+ */
+public class FirstRedisLock {
+}
